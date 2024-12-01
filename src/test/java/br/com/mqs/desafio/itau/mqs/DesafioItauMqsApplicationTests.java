38d6425c1f8ffca063d68621a753587112eedaf7
Marcelo Marques de Lima <111276703+MarceloMarquesdeLima@users.noreply.github.com>
2024-12-01 09:52:49 -0300

    Inicio desafio Itau
    Criando um desafio de teste Itaú

---
+++ src/test/java/br/com/mqs/desafio/itau/mqs/DesafioItauMqsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mqs.desafio.itau.mqs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioItauMqsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
